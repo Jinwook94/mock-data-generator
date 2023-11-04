e4ee138525207ccb3272b02d76f4414f1b4b1280
Jinwook Kim <kimjinwook94@gmail.com>
2023-11-04 18:33:01 +0900

    측정시간 표 출력 추가

--- mariadb_to_elasticsearch.py
+++ mariadb_to_elasticsearch.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from elasticsearch import Elasticsearch, helpers
 from decouple import config
 from db_connector import get_db_connection
+import time
+from prettytable import PrettyTable
 
 # Elasticsearch 클라이언트 설정
 es = Elasticsearch(
@@ -19,22 +21,29 @@ cursor = mydb.cursor(dictionary=True)
 # Elasticsearch 인덱스명
 es_index = 'post'
 
-# 한 번에 처리할 레코드 수를 정의합니다. 여기서는 10,000으로 설정합니다.
-batch_size = 10000  # Adjust the batch size as appropriate
+# 한 번에 처리할 레코드 수를 정의
+batch_size = 10000
+
+# 작업 시간 측정 위한 딕셔너리
+time_dict = {
+    'Elasticsearch에 데이터 인덱싱': 0,
+}
 
 try:
-    # 총 레코드 수를 가져옵니다.
+    # 총 레코드 수 가져옴
     cursor.execute("SELECT COUNT(*) FROM post")
     total_count = cursor.fetchone()['COUNT(*)']
     print(f"Total records to process: {total_count}")
 
-    # 처리할 전체 페이지 수를 계산합니다.
+    # 처리할 전체 페이지 수를 계산
     pages = total_count // batch_size + (1 if total_count % batch_size else 0)
+
+    indexing_start_time = time.time()
 
     for page in range(pages):
         offset = page * batch_size
 
-        # JOIN 쿼리를 사용하여 필요한 정보를 가져옵니다.
+        # JOIN 쿼리를 사용하여 필요한 정보를 가져옴
         query = """
         SELECT p.*, m.nickname AS writerNickname, m.profile_image AS writerProfileImage, 
                (SELECT COUNT(*) FROM comment c WHERE c.post_id = p.post_id) AS commentCount
@@ -45,7 +54,7 @@ try:
         cursor.execute(query, (batch_size, offset))
         rows = cursor.fetchall()
 
-        # 각 레코드에 대한 액션을 생성합니다.
+        # 각 레코드에 대한 액션을 생성
         actions = [
             {
                 "_index": es_index,
@@ -70,9 +79,12 @@ try:
             for row in rows
         ]
 
-        # Bulk 인덱싱을 실행합니다.
+        # Bulk 인덱싱 실행
         helpers.bulk(es, actions)
         print(f"Batch {page + 1}/{pages} indexed.")
+
+        indexing_end_time = time.time()
+        time_dict['Elasticsearch에 데이터 인덱싱'] = indexing_end_time - indexing_start_time
 
 except Exception as e:
     print(f"Error: {e}")
@@ -81,3 +93,15 @@ finally:
     if mydb.is_connected():
         cursor.close()
         mydb.close()
+
+    # 총 소요 시간을 계산하여 추가
+    total_time = sum(time_dict.values())
+    time_dict['Total'] = total_time
+
+    # 소요시간 측정 표 출력
+    table = PrettyTable()
+    table.field_names = ["작업", "소요 시간 (초)"]
+    for key, value in time_dict.items():
+        table.add_row([key, f"{value:.2f}"])
+
+    print(table)
